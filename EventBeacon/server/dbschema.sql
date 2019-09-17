89aabf66976d92b9b0a64919bcddad124a55eaa7
Suleimi Ahmed <suleimi.ahmed@indexexchange.com>
2019-09-17 17:16:16 -0400

    added db schema

---
+++ EventBeacon/server/dbschema.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE  IF NOT EXISTS `go-mysql-crud` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `go-mysql-crud`;
+
+/* Create table for `events` */
+
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE `events` (
+
+  `event_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `description` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `location` longtext COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`event_id`)
+);
+
+/* Create table for `event_subscription` */
+DROP TABLE IF EXISTS `event_subscription`;
+CREATE TABLE `event_subscription` (
+
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `event_id` int(11),
+  PRIMARY KEY (`user_id`),
+  FOREIGN KEY (event_id) REFERENCES events(event_id)
+);
