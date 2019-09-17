7fb0e9274c5556526e592a5cfb8247f7a3bf9574
Jason Ku <jason.ku@mac-1893-jk.local>
2019-09-17 16:11:55 -0400

    Adding form

--- EventBeacon/src/app/app.component.ts
+++ EventBeacon/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'EventBeacon';
+@Component({ selector: 'app-root', templateUrl: 'app.component.html' })
+export class AppComponent implements OnInit {
+    registerForm: FormGroup;
+    submitted = false;
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit() {
+        this.registerForm = this.formBuilder.group({
+            title: ['', Validators.required],
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]]
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        // display form values on success
+        alert('Successfully submitted event');
+    }
+
+    onReset() {
+        this.submitted = false;
+        this.registerForm.reset();
+    }
 }
