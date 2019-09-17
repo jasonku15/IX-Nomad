6d81e8280d7a69bf38386b8e532ef1d0dbc0fbbe
Jason Ku <jason.ku@mac-1893-jk.local>
2019-09-17 18:48:08 -0400

    Refactoring in to components

--- EventBeacon/src/app/app.component.ts
+++ EventBeacon/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-@Component({ selector: 'app-root', templateUrl: 'app.component.html' })
+@Component({ selector: 'app-root', templateUrl: 'app.component.html', styleUrls: ['./app.component.css'] })
 export class AppComponent implements OnInit {
     registerForm: FormGroup;
     submitted = false;
